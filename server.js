5b04b249685bf239677a4caf46d31d55ec004b8c
Brian Eddow <brian.eddow@gmail.com>
2016-05-29 00:05:03 -0700

    removed models/ctrls

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./client/static")));
 
 // require('./server/config/mongoose.js');
--- server/models/customer.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var CustomerSchema = new mongoose.Schema({
-  name: {type: String, unique: true},
-  created_at: {type: Date, default: Date()}
-})
-
-var Customers = mongoose.model('Customers', CustomerSchema);
